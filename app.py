49494a5c69031026a73d5c49c04d2a59962d9422
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2020-09-12 06:32:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from models.post import Post
 Database.initialize()
 
 post = Post('Post #1', 'Learn Python', 'T.J')
-post2 = Post('Post #2', 'Learn 2 Hack!!!', 'T.J')
+post2 = Post('Post #2', 'Learn MongoDB!!!', 'T.J')
 
 
 print(post.content)
